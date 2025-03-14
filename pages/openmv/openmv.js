42baeb57083ef3e6c31839cbe5f790816ddccd5b
98yearsoldshark <1839369370@qq.com>
2025-03-14 21:58:14 +0800

    openmv

--- pages/openmv/openmv.js
+++ pages/openmv/openmv.js
@@ -1,66 +1,13 @@
-// pages/openmv/openmv.js
-Page({
+// start.js
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  //跳转到天气页面
+  navigate: function() {
+      wx.navigateTo({
+          url: '../wifi_station/tianqi/tianqi',
+      })
   }
 })
